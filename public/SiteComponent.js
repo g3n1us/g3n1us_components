5b0d7fe6833296b40f290917006e004b398d0f8f
Sean Bethel <sbethel@mediadc.com>
2016-11-03 14:41:43 -0400

    Add ability to nest components and also overridable file_version ing

--- public/SiteComponent.js
+++ public/SiteComponent.js
@@ -15,8 +15,9 @@ function SiteComponent(component_name, options){
 	_this.dataFromDOM = null; // can be function that parses existing DOM to provide data
 	_this.trigger_event = null;
 	_this.on_event = null;
+	_this.file_version = file_version || '1';
 	_this.required_templates = {
-		'index': 'index.hbs',
+		'index': 'index.hbs?v='+_this.file_version,
 	};
 	
 	// 	apply user defined options
@@ -30,31 +31,38 @@ function SiteComponent(component_name, options){
 	_this.directory = _this.script.getAttribute('src').split('SiteComponent.js')[0] + component_name;
 	_this.directory_url = _this.script.src.split('SiteComponent.js')[0] + component_name;	
 	_this.pathArray = _this.directory.split('/');
+	
 
 	// Init the script
 	_this.run = function(){
 		_this.wait(function(){			
-			$.getScript(_this.directory_url + '/index.js', function(){
+			$.getScript(_this.directory_url + '/index.js?v='+_this.file_version, function(){
 				_this.getData(function(){
 					// call CSS by itself, runs async
 					_this.putCSS(function(){});	
 						
 					if(_this.dataTransformer) _this.dataTransformer();	
-					console.log(_this.data);	
+					console.log(_this.data);
 					_this.required_js_length = Object.keys(_this.required_js).length;				
 					_this.required_css_length = Object.keys(_this.required_css).length;
 					_this.required_templates_length = Object.keys(_this.required_templates).length;
 					_this.getTemplates(function(){
 						var completed = 0;
 						if(!_this.required_js_length) {
-							 _this.callback(_this.data);
-							 $('.mdc_component--loading').removeClass('.mdc_component--loading');
+							var event = document.createEvent('Event');
+							event.initEvent('mdc_components_loaded', true, true);
+							document.dispatchEvent(event);	        		    
+							_this.callback(_this.data);							    
+// 							$('.mdc_component--loading').removeClass('.mdc_component--loading');
 						}
 						_this.getScripts(function(textStatus){
 							if(textStatus === 'success') completed++;
 							if(completed == _this.required_js_length){
-								 _this.callback(_this.data);
-								 $('.mdc_component--loading').removeClass('.mdc_component--loading');
+								var event = document.createEvent('Event');
+								event.initEvent('mdc_components_loaded', true, true);
+								document.dispatchEvent(event);	        		    
+								 _this.callback(_this.data);								    								 
+// 								$('.mdc_component--loading').removeClass('.mdc_component--loading');
 							}
 						});
 					});
@@ -96,17 +104,20 @@ function SiteComponent(component_name, options){
 			_this.data = _this.dataFromDOM();
 			callback();
 		}
-		else $.getJSON(_this.data_url, {'component_name': _this.namespace}, function(data){			
-			console.log(data);
-			_this.data = data;
-			callback();
-		})
-		.fail(function(resp) {
-			console.log(resp);
-		})
-		.always(function() {
-			console.log("always");
-		});
+		else {
+			_this.data_url = (_this.data_url[0] == '/' || _this.data_url.slice(0,4) == 'http') ? _this.data_url : _this.directory+'/'+_this.data_url;
+			$.getJSON(_this.data_url, {'component_name': _this.namespace}, function(data){			
+				console.log(data);
+				_this.data = data;
+				callback();
+			})
+			.fail(function(resp) {
+				console.log(resp);
+			})
+			.always(function() {
+				console.log("always");
+			});			
+		}
 	}
 	
 	
@@ -143,6 +154,8 @@ function SiteComponent(component_name, options){
 		for(var key in _this.required_css){
 			var val = _this.required_css[key];
 			var path = (val[0] == '/' || val.slice(0,4) == 'http') ? val : _this.directory+'/'+val;
+			if(path.indexOf('?') === -1) path = path + '?v=' + _this.file_version;
+			else path = path + '&v=' + _this.file_version;
 			var completed = 0;
 			$.get(path, function(css){
 				completed++;
@@ -170,6 +183,8 @@ function SiteComponent(component_name, options){
 				for(var key in _this.required_templates){
 					var val = _this.required_templates[key];
 					var path = val[0] == '/' ? val : _this.directory+'/'+val;
+					if(path.indexOf('?') === -1) path = path + '?v=' + _this.file_version;
+					else path = path + '&v=' + _this.file_version;					
 					var key = key;					
 					template_requests[key] = $.get(path, function(hb, textStatus, xhr){
 						completed++;
@@ -184,9 +199,10 @@ function SiteComponent(component_name, options){
 							var $targetdiv = _this.domtarget;
 							if(_this.domtarget && typeof _this.domtarget === "string")
 								var $targetdiv = $(_this.domtarget);
-							var $rendered = $(_this.templates.index(_this.data));	
+// 							var $rendered = $(_this.templates.index(_this.data));	
+							var rendered = _this.templates.index(_this.data);	
 							if($targetdiv[0].tagName.toLowerCase() == "script"){
-								$targetdiv.before($rendered);
+								$targetdiv.before(rendered);
 							}
 							else{
 								if(_this.domtarget_index != null){
@@ -195,14 +211,14 @@ function SiteComponent(component_name, options){
 									else _this.domtarget_index = parseInt(_this.domtarget_index);
 									$targetdiv = $targetdiv.eq(_this.domtarget_index);
 								}
-								$targetdiv.html($rendered);
+								$targetdiv.html(rendered);
 							}
 							_this.container = $targetdiv;
-							$targetdiv.add($rendered);
+							$targetdiv.add(rendered);
 							$targetdiv.removeClass('hidden hide mdc_component--loading')
 								.addClass('mdc_component--loaded')
 								.show();					
-
+							
 							callback();								
 						}
 					})
@@ -221,6 +237,9 @@ function SiteComponent(component_name, options){
 		for(var key in _this.required_js){
 			var val = _this.required_js[key];
 			var path = val[0] == '/' ? val : _this.directory+'/'+val;
+			if(path.indexOf('?') === -1) path = path + '?v=' + _this.file_version;
+			else path = path + '&v=' + _this.file_version;		
+			
 			if(val.indexOf('handle') !== -1){
 				var myScript = document.createElement('script');
 				myScript.src = path;				
@@ -241,7 +260,7 @@ function SiteComponent(component_name, options){
 
 
 document.addEventListener('mdc_components_loaded', function(e){
-	$("[data-mdc_component]").each(function(){
+	$("[data-mdc_component]").not('.mdc_component--loading, .mdc_component--loaded').each(function(){
 		var component_name = $(this).data('mdc_component');
 		window[component_name];
 		var data = $(this).data();
